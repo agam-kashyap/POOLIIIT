1bf0f12542be6bb313c8aea18119953e26daf7b0
agam-kashyap <agamkashyap2018@gmail.com>
2019-12-25 20:51:21 +0530

    Relationship Added in Django

--- backend/pool/models.py
+++ backend/pool/models.py
@@ -1,18 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Booking(models.Model):
-    firstname = models.CharField(max_length=128)
-    lastname = models.CharField(max_length=128)
-    email = models.EmailField(max_length = 254)
-    time = models.TimeField()
-    flexibility_before = models.TimeField()
-    flexibility_after = models.TimeField()
-    date = models.DateField()
-
-    def __str__(self):
-        return self.firstname+" "+self.lastname
-
 
 class Register(models.Model):
     firstname = models.CharField(max_length=128)
@@ -21,6 +9,23 @@ class Register(models.Model):
     password = models.CharField(max_length=50)
     username = models.CharField(max_length=128)
     phone = models.CharField(max_length=10)
+    id = models.IntegerField(primary_key=True)
 
     def __str__(self):
     	return self.username
+class Booking(models.Model):
+    time = models.TimeField()
+    flexibility_before = models.TimeField()
+    flexibility_after = models.TimeField()
+    date = models.DateField()
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(
+        to='Register', 
+        on_delete=models.CASCADE,
+        related_name='user',
+        null=True,
+        blank=True
+        )
+
+    def __str__(self):
+        return self.firstname+" "+self.lastname
